98cdd0b909f0369c61ed95846d159dea00a0adf3
Spliterash <me@spliterash.ru>
2020-09-04 20:39:01 +0400

    Добавил пятое задание

---
+++ src/ru/spliterash/javatasks/t5/T5.java
@@ -0,0 +1,13 @@
+package ru.spliterash.javatasks.t5;
+
+/**
+ * Необходимо, чтоб программа выводила на экран вот такую последовательность:
+ * 7 14 21 28 35 42 49 56 63 70 77 84 91 98
+ */
+public class T5 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 15; i++) {
+            System.out.println((i * 7));
+        }
+    }
+}
