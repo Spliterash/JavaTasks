b47d6b23fb093a90d3e091b19d56c6daaf7ec459
Spliterash <me@spliterash.ru>
2020-09-04 20:25:21 +0400

    Третье задание

--- src/ru/spliterash/javatasks/second/Second.java
+++ src/ru/spliterash/javatasks/second/Second.java
@@ -1,5 +1,9 @@
 package ru.spliterash.javatasks.second;
 
+/**
+ * Необходимо вывести на экран числа от 5 до 1. На экране должно быть:
+ * 5 4 3 2 1
+ */
 public class Second {
     public static void main(String[] args) {
         for(int i = 5;i>0;i--){
---
+++ src/ru/spliterash/javatasks/third/Third.java
@@ -0,0 +1,9 @@
+package ru.spliterash.javatasks.third;
+
+public class Third {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("3*%d=%d%n", i, 3 * i);
+        }
+    }
+}
