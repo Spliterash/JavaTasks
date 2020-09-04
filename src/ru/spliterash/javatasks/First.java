1eae249113c1ce85743cd1102c4d5ab8e2131b6f
Spliterash <me@spliterash.ru>
2020-09-04 20:12:13 +0400

    Initial commit

---
+++ src/ru/spliterash/javatasks/First.java
@@ -0,0 +1,13 @@
+package ru.spliterash.javatasks;
+
+/**
+ * Необходимо вывести на экран числа от 1 до 5. На экране должно быть:
+ * 1 2 3 4 5
+ */
+public class First {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i + 1);
+        }
+    }
+}
