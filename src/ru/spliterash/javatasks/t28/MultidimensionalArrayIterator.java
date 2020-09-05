3ac06046a2c0ad761323d337f5e3ec4e88c96b92
Spliterash <me@spliterash.ru>
2020-09-05 17:24:46 +0400

    Добавил 28 задание

---
+++ src/ru/spliterash/javatasks/t28/MultidimensionalArrayIterator.java
@@ -0,0 +1,48 @@
+package ru.spliterash.javatasks.t28;
+
+import java.util.Iterator;
+
+/**
+ * Напишите итератор по двумерному массиву.
+ */
+public class MultidimensionalArrayIterator<T> implements Iterator<T> {
+    private final T[][] array;
+    private int x = -1;
+    private int y = 0;
+
+    public MultidimensionalArrayIterator(T[][] array) {
+        this.array = array;
+
+    }
+
+    public boolean next(boolean moveCounter) {
+        if (y < array.length) {
+            T[] xArray = array[y];
+            if (x + 1 < xArray.length) {
+                if (moveCounter)
+                    x += 1;
+                return true;
+            } else if (y + 1 < array.length) {
+                if (moveCounter) {
+                    y += 1;
+                    x = 0;
+                }
+                return true;
+            } else
+                return false;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return next(false);
+    }
+
+    @Override
+    public T next() {
+        if (!next(true))
+            throw new IllegalArgumentException("Next does not exists");
+        return array[y][x];
+    }
+}
---
+++ src/ru/spliterash/javatasks/t28/T28.java
@@ -0,0 +1,16 @@
+package ru.spliterash.javatasks.t28;
+
+public class T28 {
+    public static void main(String[] args) {
+        Integer[][] array = new Integer[][]{
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12, 13},
+                {14, 15}
+        };
+        for (MultidimensionalArrayIterator<Integer> iter = new MultidimensionalArrayIterator<>(array); iter.hasNext(); ) {
+            Integer next = iter.next();
+            System.out.println(next);
+        }
+    }
+}
