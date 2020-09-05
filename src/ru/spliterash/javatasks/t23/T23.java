91a02fd3d5b4866011767ddf2525d8e74891fb96
Spliterash <me@spliterash.ru>
2020-09-05 15:30:54 +0400

    Добавил описание

--- src/ru/spliterash/javatasks/t23/T23.java
+++ src/ru/spliterash/javatasks/t23/T23.java
@@ -1,5 +1,11 @@
 package ru.spliterash.javatasks.t23;
 
+/**
+ * Напишите свой класс StringBuilder с поддержкой операции undo.
+ *
+ * ﻿Для этого делегируйте все методы стандартному StringBuilder,
+ * а в собственном классе храните список всех операций для выполнения undo(). Это будет реализацией шаблона «Команда».
+ */
 public class T23 {
     public static void main(String[] args) {
         MyStringBuilder builder = new MyStringBuilder();
